7b17ba4cd8beda2f50a9789ce2409155a9606ad1
bagusprakasa <47969692+bagusprakasa@users.noreply.github.com>
2022-09-22 09:05:39 +0700

    CRUD category

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 var categoryRouter = require("./app/category/router");
 var dashboardRouter = require("./app/dashboard/router");
@@ -13,6 +16,15 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+app.use(methodOverride("_method"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- app/category/router.js
+++ app/category/router.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var router = express.Router();
-const { index, create, store } = require("./controller");
+const { index, create, store, edit, update, destroy } = require("./controller");
 
 /* GET users listing. */
 router.get("/", index);
 router.get("/create", create);
 router.post("/store", store);
+router.get("/edit/:id", edit);
+router.put("/:id", update);
+router.delete("/:id", destroy);
 
 module.exports = router;
